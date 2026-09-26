36aa7fa50237258b952b0019e72f0c0a5958eb1c
juaozin01 <pjoao8355@gmail.com>
2026-09-26 00:08:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-function mostrarMensagem(projeto) {
-    alert("Você clicou no projeto: " + projeto);
+function somar() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = numero1 + numero2;
+
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
+}
+function subtrair() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = numero1 - numero2;
+
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
+}
+function multiplicar() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = numero1 * numero2;
+
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
+}
+function dividir() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = numero1 / numero2;
+
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
+}
+function limpar() {
+    document.getElementById("numero1").value = "";
+    document.getElementById("numero2").value = "";
+    document.getElementById("resultado").innerText = "Resultado:";
 }
